12236dcad988e58091ad05af4990c240a6671576
lurd4862 <stefan.fouche.ltd@gmail.com>
2020-12-16 00:33:29 +0200

    started setting up notebooks for different testing ideas

--- test_conda_env.py
+++ test_conda_env.py
@@ -1,10 +1,11 @@
 import gym
-
-env = gym.make('CartPole-v0')
-env.reset()
-
+env = gym.make("CartPole-v1")
+observation = env.reset()
 for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample())
+  env.render()
+  action = env.action_space.sample() # your agent here (this takes random actions)
+  observation, reward, done, info = env.step(action)
 
+  if done:
+    observation = env.reset()
 env.close()
